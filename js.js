d2982ea4631f42fb3a3e482a1deafa173e93193b
evgen5382 <62152758+evgen5382@users.noreply.github.com>
2020-08-26 11:39:56 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -3,11 +3,14 @@
   let inp1 = document.querySelector('.inp-1');
   let inp2 = document.querySelector('.inp-2');
   let inp3 = document.querySelector('.inp-3');
+  let inp4 = document.querySelector('.inp-4');
   
   let out = +inp1.value * 15000 * +inp2.value;
   // console.log(out)
   let out0 = (out / 100) * +inp3.value;
   // console.log(out0)
-  document.querySelector('.out').innerHTML = out + out0;
+  let out1 = (out + out0) / 100 * +inp4.value
+  // console.log(out1)
+  document.querySelector('.out').innerHTML = out + out0 + out1;
 }
 document.querySelector('.btn').onclick = f1;
