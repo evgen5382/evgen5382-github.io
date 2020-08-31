fe5d8c7647e9be4700a31597fbdf32837cfca043
evgen5382 <62152758+evgen5382@users.noreply.github.com>
2020-08-31 18:34:13 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,16 +1,16 @@
  function f1 (){
 
   let inp1 = document.querySelector('.inp-1');
-  let inp2 = document.querySelector('.inp-2');
-  let inp3 = document.querySelector('.inp-3');
-  let inp4 = document.querySelector('.inp-4');
+  let s1 = document.querySelector('.s-1');
+  let s2 = document.querySelector('.s-2');
+  let s3 = document.querySelector('.s-3');
   
-  let out = +inp1.value * 15000 * +inp2.value;
+  let out1 = +inp1.value * 15000 * +s1.value;
   // console.log(out)
-  let out0 = (out / 100) * +inp3.value;
-  // console.log(out0)
-  let out1 = (out + out0) / 100 * +inp4.value
+  let out2 = +s2.value + +s3.value;
+  let out3 = out1/100 * out2;
+  console.log(out3)
   // console.log(out1)
-  document.querySelector('.out').innerHTML = out + out0 + out1;
+  document.querySelector('.out').innerHTML = out1 + out3;
 }
 document.querySelector('.btn').onclick = f1;
